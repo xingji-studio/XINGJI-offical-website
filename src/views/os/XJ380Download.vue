32088e750adca1ba50ef2d41e01c093e43be476d
Guoqiyu1115 <guoqiyu1115@163.com>
2026-05-14 18:32:08 +0800

    升级资料

--- src/views/os/XJ380Download.vue
+++ src/views/os/XJ380Download.vue
@@ -150,7 +150,7 @@ const requirements2 = ref([
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>
               </a>
-              <a :href="tool.backupUrl"
+              <a :href="tool.origin_url"
                  class="inline-flex items-center px-4 py-2 rounded-lg bg-gray-500/20 text-gray-400 hover:bg-gray-500/30 transition-colors duration-200 text-sm">
                 官网链接下载（稳定但慢，优先用前俩）
                 <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
